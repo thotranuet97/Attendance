e0b8c791cc0e6af5c7f78c0533a6fd8378cbffb0
thotranuet97 <thotranuet97@gmail.com>
2017-09-30 16:29:06 +0700

    update statistics

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -3,29 +3,36 @@ class Admin::StatisticsController < ApplicationController
   before_action :require_admin
 
   def index
-    query_month = Attendance.connection.select_all(
-      "SELECT DISTINCT extract(month FROM a.date) AS month
-         FROM attendances AS a 
-         WHERE a.time_out IS NOT NULL"
-      )
-    @month = query_month.rows
+    @tmp = 0 
+    @users = User.all
 
-    query_year = Attendance.connection.select_all(
-    	"SELECT DISTINCT extract(year FROM a.date) AS year
-         FROM attendances AS a 
-         WHERE a.time_out IS NOT NULL"
-    	)
-    @year = query_year.rows
-
-    @test = true
-    time = 8
-    
-    @total_time = Attendance.select('u.full_name as name, 
-      sec_to_time(sum(time_to_sec(timediff(time_out, time_in)))) as time')
+    @test = false
+    if !params[:month].nil? && !params[:year].nil?
+      @test = true
+      @users.each do |user| 
+        user.attendances.each do |x|
+          if (x.time_out.nil?) && x.date.mon.equal?(params[:month].to_i)
+            flash.now[:danger] = user.full_name + " " + t("admin.statistics.index.time_out_nil") 
+          end
+        end
+      end
+      if params[:month].to_i < 10
+        time = params[:year].to_s + '0' + params[:month].to_s 
+      else
+        time = params[:year].to_s + params[:month].to_s 
+      end
+    end
+   
+    @total_time = Attendance.select('u.id, u.full_name as name, 
+      sum(time_to_sec(timediff(time_out, time_in))) as time')
         .joins('inner join users as u on u.id = user_id ')
-        .where('time_out IS NOT NULL AND extract(month FROM date) = ?', time)
+        .where('time_out IS NOT NULL AND extract(year_month FROM date) = ?', time)
         .group('user_id')
+        .reorder('')  
         
+    @months = ['January','February','March','April','May','June',
+              'July','August','September','October','November','December']
   end
 
 end
+
